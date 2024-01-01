cb2e24031edb87e6db6ea3bed499e6f3788a6552
favournikade <153927678+favournikade@users.noreply.github.com>
2024-01-01 03:38:15 -0800

    Add files via upload
    Acting index,style.css&script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('open');
+};
+/*=read more=*/
+const text = document.querySelector(".btn-section");
+const viewMore =document.querySelector(".btn");
+viewMore.addEventListener("click", () => {
+    text.classList.toggle("open");
+})
+
+
+/*= scroll reveal=*/
+ScrollReveal({
+    //reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
